25a8346491a1b1c3a435bc21a351d667efb2d30f
Douglas Van Hollen <dsubnet0@gmail.com>
2011-04-26 20:45:19 -0400

    title formatting.  money

--- json3.php
+++ json3.php
@@ -1,10 +1,11 @@
 <?php
 echo "<head>\n";
-    echo ' <link rel="stylesheet" href="css/style.css" type="text/css">'."\n";
-	echo ' <link rel="stylesheet" href="css/web20.css" type="text/css">'."\n";
+
 	echo ' <script type="text/javascript" src="js/tablesort.js"></script>'."\n";
 	echo ' <script type="text/javascript" src="js/paginate.js"></script>'."\n";
 	echo ' <script type="text/javascript" src="js/dragtable.js"></script>'."\n";
+	    echo ' <link rel="stylesheet" href="css/style.css" type="text/css">'."\n";
+	echo ' <link rel="stylesheet" href="css/web20.css" type="text/css">'."\n";
 echo "</head>\n";
 
 $jsonurl = "http://apps.pionline.com/RCTest/dj.asp";
@@ -12,19 +13,20 @@ $json = file_get_contents($jsonurl,0,null,null);
 $json_output = json_decode($json);
 $fields = array("2660_1", "31967_1", "31971_1", "31975_1");
 
-//print_r($json_output);
-//echo '<table align="center">\n';
-//echo '<table align="center" class="sortable-onload-3 no-arrow rowstyle-alt colstyle-alt paginate-50 max-pages-7 draggable">'."\n";
-/*echo "<th>Company Name</th>\n";
-echo "<th>Worldwide Assets Under Management (AUM)</th>\n";
-echo "<th>Total Equity Assets</th>\n";
-echo "<th>Total Fixed Income Assets</th>\n";
-echo "<th>Total Alternative Assets</th>\n";
-*/
 ?>
-<caption>(Click and drag on column titles to reorder)</caption>
-<table align="center" class="sortable-onload-3 no-arrow rowstyle-alt colstyle-alt paginate-50 max-pages-7 draggable">
 
+<body>
+<div id="container">
+<div id="title">
+<a href="json3.php"><img src="img/pilogo.png" width="379" height="69"></a>
+<p class="subtitle">Tabular Reference Area</p>
+</div>
+<div id="copy">
+<p class="copy">This section of the site hosts an interactive chart of financial stats gathered from our 100 most popular financial companies.</p>
+<p class="instructions">CLICK on a column heading to sort.  Or DRAG to rearrange the columns.</p>
+</div>
+
+<table class="sortable-onload-3 no-arrow rowstyle-alt colstyle-alt paginate-50 max-pages-7 draggable">
 <thead>
 <tr>
 	<th class="sortable-text">Company Name</th>
@@ -38,14 +40,15 @@ echo "<th>Total Alternative Assets</th>\n";
 <?php
 foreach ( $json_output->BODY as $body )
 {
-    //echo "$body->NAME<br>";
-
 	echo "<tr>\n";
-	echo "<td>" . $body->NAME . "</td>\n";
+	echo '<td class="name">' . $body->NAME . '</td>' . "\n";
 	foreach ($fields as $field) {
-		echo "<td>" . $body->DATA->$field . "</td>\n";
+		echo '<td class="data">' . number_format($body->DATA->$field, 2) . '</td>' . "\n";
 	}
 	echo "</tr>\n";
 }
 echo "</table>\n";
+
 ?>
+</div>
+</body>
